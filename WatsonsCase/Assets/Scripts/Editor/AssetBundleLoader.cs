157e75db3bb6723c09f4cd83088d119836973832
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2018-10-05 01:26:01 +0200

    Added Simple AssetBundle Maker And Loader Scripts to Setup for level exporting/loading

---
+++ WatsonsCase/Assets/Scripts/Editor/AssetBundleLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleLoader : EditorWindow
+{
+
+
+    [MenuItem("LevelEditor/AssetBundleLoader")]
+    static void Init()
+    {
+        AssetBundleLoader window = (AssetBundleLoader)EditorWindow.GetWindow(typeof(AssetBundleLoader));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        
+        GUILayout.Label("Select Path to load AssetBundle", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("Load Asset Bundle"))
+        {
+            string path = EditorUtility.OpenFilePanel("Please Select the Asset Bundle", "", "");
+            Debug.Log(path);
+            AssetBundle ab =  AssetBundle.LoadFromFile(path);
+            foreach (var scene in ab.GetAllScenePaths())
+            {
+                Debug.Log(scene);
+            }
+            
+        }
+    }
+
+    
+}
---
+++ WatsonsCase/Assets/Scripts/Editor/AssetBundleMaker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleMaker : EditorWindow
+{
+
+    Object scene;
+
+    [MenuItem("LevelEditor/AssetBundleMaker")]
+    static void Init()
+    {
+        AssetBundleMaker window = (AssetBundleMaker)EditorWindow.GetWindow(typeof(AssetBundleMaker));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        
+        GUILayout.Label("Select Object to Create AssetBundle", EditorStyles.boldLabel);
+        scene = EditorGUILayout.ObjectField(scene, typeof(Object), true);
+
+       
+        if(GUILayout.Button("Create Asset Bundles")){
+            string assetPath = AssetDatabase.GetAssetPath(scene);
+            AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant("testBundle", "");
+
+            BuildPipeline.BuildAssetBundles("Assets/", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+            
+        }
+    }
+
+    
+}
